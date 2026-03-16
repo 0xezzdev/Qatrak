7d226c2e309401745c442f361787f44cb63ee3e5
Ezzeldeen Mohamed Abdelwahab <ezzeldeen20052018@gmail.com>
2026-03-16 23:19:43 +0200

    add train list screen

---
+++ lib/core/model/train_model.dart
@@ -0,0 +1,25 @@
+class TrainModel {
+  final String id;
+  final String trainNumber;
+  final String trainType;
+  final String route;
+  final String direction;
+
+  TrainModel({
+    required this.id,
+    required this.trainNumber,
+    required this.trainType,
+    required this.route,
+    required this.direction,
+  });
+
+  factory TrainModel.fromJson(Map<String, dynamic> json) {
+    return TrainModel(
+      id: json['id'].toString(),
+      trainNumber: json['train_number'].toString(),
+      trainType: json['train_type'].toString(),
+      route: json['route'].toString(),
+      direction: json['direction'].toString(),
+    );
+  }
+}
