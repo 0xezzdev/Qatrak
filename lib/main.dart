8dd4b51a9dec30452350bd582ebeaf325fe575ad
Ezzeldeen Mohamed Abdelwahab <ezzeldeen20052018@gmail.com>
2026-03-18 04:23:34 +0200

    check internet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:qatrak/core/colors/app_colors.dart';
+import 'package:qatrak/core/widget/custom_button.dart';
 import 'package:qatrak/feature/splash_screen/widget/splash_screen.dart';
 import 'package:qatrak/services/supabase_service.dart';
 
@@ -27,8 +30,99 @@ class MyApp extends StatelessWidget {
           //locale: DevicePreview.locale(context),
           //builder: DevicePreview.appBuilder,
           home: SplashScreen(),
+          builder: (context, child) {
+            return NetworkWrapper(child: child!);
+          },
         );
       },
+    );
+  }
+}
+
+class NetworkWrapper extends StatefulWidget {
+  final Widget child;
+  const NetworkWrapper({super.key, required this.child});
+
+  @override
+  State<NetworkWrapper> createState() => _NetworkWrapperState();
+}
+
+class _NetworkWrapperState extends State<NetworkWrapper> {
+  bool _isOffline = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Connectivity().onConnectivityChanged.listen((
+      List<ConnectivityResult> result,
+    ) {
+      setState(() {
+        _isOffline = result.contains(ConnectivityResult.none);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        widget.child,
+        if (_isOffline)
+          Material(
+            type: MaterialType.transparency,
+            child: Container(
+              color: Colors.black.withValues(alpha: 0.7),
+              child: Center(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30.w),
+                  padding: EdgeInsets.all(20.sp),
+                  decoration: BoxDecoration(
+                    color: AppColors.background,
+                    borderRadius: BorderRadius.circular(20.r),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.cloud_off_rounded,
+                        size: 70.sp,
+                        color: AppColors.primary,
+                      ),
+                      SizedBox(height: 20.h),
+                      Text(
+                        "No Internet Connection",
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.foreground,
+                        ),
+                      ),
+                      SizedBox(height: 10.h),
+                      Text(
+                        "Please check your connection and try again.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: AppColors.textHint,
+                          fontSize: 14.sp,
+                        ),
+                      ),
+                      SizedBox(height: 30.h),
+                      CustomButton(
+                        text: "Retry Connection",
+                        onPressed: () async {
+                          var result = await Connectivity().checkConnectivity();
+                          if (!result.contains(ConnectivityResult.none)) {
+                            setState(() => _isOffline = false);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
